5092ca8921a223e06b5ffc710774cf5d20142a9e
prsndp <prasandeepp@gmail.com>
2022-09-11 14:31:11 +0530

    DOM practice done

---
+++ DOM/app.js
@@ -0,0 +1,21 @@
+// DOM Manipulation
+
+
+// get Element By Id()
+
+const title = document.getElementById('main-heading');
+console.log(title);
+
+// getelementByclassname()
+
+const listItems = document.getElementsByTagName('li');
+
+console.log(listItems);
+
+
+const container =document.querySelectorAll('div');
+
+console.log(container);
+
+
+
